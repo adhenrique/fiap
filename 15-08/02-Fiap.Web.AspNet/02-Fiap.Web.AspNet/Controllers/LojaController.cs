411eb9d36bd0f706bd788d70aa0daaa5ad38f110
Logon Aluno <logonaluno@local.com>
2019-08-15 22:42:00 -0300

    mvc basico com recebimento e envio de parametros pela controller

---
+++ 15-08/02-Fiap.Web.AspNet/02-Fiap.Web.AspNet/Controllers/LojaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _02_Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_Fiap.Web.AspNet.Controllers
+{
+    public class LojaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpGet] // abre a pagina do form
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost] // recupera os dados do form
+        public IActionResult Cadastrar(Loja loja)
+        {
+            // envia valores para view
+            ViewData["sergio"] = loja.Nome;
+            ViewBag.churros = loja.Faturamento;
+
+            // sobrevive após um redirect
+            TempData["msg"] = "Cadastrado!";
+            return View();
+        }
+    }
+}
---
+++ 15-08/02-Fiap.Web.AspNet/02-Fiap.Web.AspNet/Models/Loja.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02_Fiap.Web.AspNet.Models
+{
+    public class Loja
+    {
+        public String Nome { get; set; }
+        public String Endereco { get; set; }
+        public decimal Faturamento { get; set; }
+    }
+}
