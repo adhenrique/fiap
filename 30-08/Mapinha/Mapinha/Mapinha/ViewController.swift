b5dc56c8bfe0ca8bf602d1610b919278d78f9ccb
Adenilton Henrique Ribeiro <adhenriquemusic@gmail.com>
2019-08-30 22:49:47 -0300

    tracando rotas

--- 30-08/Mapinha/Mapinha/Mapinha/ViewController.swift
+++ 30-08/Mapinha/Mapinha/Mapinha/ViewController.swift
@@ -32,9 +32,65 @@ class ViewController: UIViewController {
 
 extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        //
+        searchBar.resignFirstResponder()
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchBar.text
+        request.region = mapView.region
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if error == nil {
+                self.mapView.removeAnnotations(self.mapView.annotations)
+                guard let response = response else {return}
+                for item in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = item.placemark.coordinate
+                    annotation.title = item.name
+                    annotation.subtitle = item.url?.absoluteString
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        }
     }
 }
 extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKPolyline {
+            
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.lineWidth = 7.0
+            renderer.strokeColor = UIColor.red
+            return renderer
+            
+        } else {
+            return MKOverlayRenderer(overlay: overlay)
+        }
+    }
     
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        let request = MKDirections.Request()
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: view.annotation!.coordinate))
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: mapView.userLocation.coordinate))
+        
+        let directions = MKDirections(request: request)
+        directions.calculate { (response, error) in
+            if error == nil {
+                guard let response = response else {return}
+                guard let route = response.routes.first else {return}
+                
+                print("Nome:", route.name)
+                print("Distância:", route.distance)
+                print("Duração:", route.expectedTravelTime)
+                
+                for step in route.steps {
+                    print("Em", step.distance, "metros", step.instructions)
+                }
+                
+                self.mapView.removeOverlays(self.mapView.overlays)
+                self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
+            }
+        }
+        
+    }
 }
