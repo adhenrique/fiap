b1e151843ac30fed1d79fc85698b8380b5d5ba2b
Adenilton Henrique Ribeiro <logonaluno@local.com>
2019-09-04 22:36:19 -0300

    metodo excluir

--- 04-09/03-aula/src/main/java/br/com/fiap/gravadora/controller/GravadoraController.java
+++ 04-09/03-aula/src/main/java/br/com/fiap/gravadora/controller/GravadoraController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("gravadora")
@@ -27,14 +29,32 @@ public class GravadoraController {
     }
 
     @PostMapping("cadastrar")
-    public String cadastrar(Gravadora gravadora, Model model){
+    public String cadastrar(Gravadora gravadora, RedirectAttributes redirectAttributes){
         // cadastrar ou salvar gravadora no banco
         rep.save(gravadora);
 
         // enviar mensagem para o front
-        model.addAttribute("msg", "Gravadora salva com sucesso!");
+        redirectAttributes.addFlashAttribute("msg", "Gravadora removida.");
 
         // retornar a pagina
-        return "home";
+        return "redirect:/gravadora/listar";
+    }
+
+    @GetMapping("editar/{id}")
+    public String editar(@PathVariable("id") int codigo, Model model) {
+        model.addAttribute("gravadora", rep.findById(codigo));
+        return "gravadora/form";
+    }
+
+    @PostMapping("excluir")
+    public String excluir(Gravadora gravadora, RedirectAttributes redirectAttributes){
+        // cadastrar ou salvar gravadora no banco
+        rep.delete(gravadora);
+
+        // enviar mensagem para o front
+        redirectAttributes.addFlashAttribute("msg", "Gravadora removida.");
+
+        // retornar a pagina
+        return "redirect:/gravadora/listar";
     }
 }
