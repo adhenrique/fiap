54db1b0db6d445a6c3f15597f043dddd0ba911c6
Adenilton Henrique Ribeiro <adhenriquemusic@gmail.com>
2019-10-03 20:48:45 -0300

    estudos sobre mapa

---
+++ 03-10/exemploMapa/app/src/main/java/br/com/fiap/exemplomapa/MapsActivity.java
@@ -0,0 +1,85 @@
+package br.com.fiap.exemplomapa;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                },
+                0
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng fiapAclimacao = new LatLng(-23.574542, -46.623528);
+        mMap.addMarker(
+                new MarkerOptions()
+                .position(fiapAclimacao)
+                .title("Fiap Aclimação")
+                .snippet("A melhor faculdade de Tecnologia")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.predion))
+        );
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(fiapAclimacao, 15));
+
+        CameraPosition cameraPosition = new CameraPosition(fiapAclimacao, 15,0, 0);
+        mMap.animateCamera(
+                CameraUpdateFactory.newCameraPosition(cameraPosition), 1000, null
+        );
+    }
+}
